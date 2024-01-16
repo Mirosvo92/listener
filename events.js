af450dd04452946a4a35b161c551e498c05b83d4
Nikita Shevchenko <nrcnekita9@gmail.com>
2024-01-16 14:16:18 +0100

    Separate 2 folders

--- events.js
+++
@@ -1,9 +0,0 @@
-const events = {
-  NewPairCreated: 'new-pair-created',
-  NewContractTransaction: 'new-contract-trans',
-  StopListenPairs: 'stop-listen-pairs',
-  ListenContractTransactions: 'listen-contract-transactions',
-  StopListenContractTransactions: 'stop-listen-contract-transactions',
-};
-
-module.exports = { events };
--- init-app.js
+++
@@ -1,7 +0,0 @@
-const { bnbController } = require('./bnb-controller');
-
-function initApp() {
-  bnbController.listenNewPairs();
-}
-
-module.exports = { initApp };
